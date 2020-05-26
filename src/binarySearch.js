cef4b1867f9d9ae3adf400fba94b757a876eb623
rchaser53 <tayoshizawa29@gmail.com>
2020-05-26 20:24:11 +0900

    return index when value equals to target value

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -4,8 +4,11 @@ const binarySearch = (arr, l, r, val) => {
   }
 
   const middle = Math.floor((l + r) / 2);
-  if (arr[middle] < val) {
+  const targetVal = arr[middle];
+  if (targetVal < val) {
     return binarySearch(arr, middle + 1, r, val);
+  } else if (targetVal === val) {
+    return middle;
   } else {
     return binarySearch(arr, l, middle, val);
   }
